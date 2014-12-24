8cb9e1d5f2b0a89ee364c2f5a146cb89fa23037b
kehribar <ihsan@kehribar.me>
2014-12-24 04:23:49 +0200

    Periodic update and voltage / temperature reading methods added.

--- commandline/t100_lib/t100_lib.cpp
+++ commandline/t100_lib/t100_lib.cpp
@@ -7,128 +7,190 @@
 #include "t100_lib.h"
 /*---------------------------------------------------------------------------*/
 uint8_t tempBuffer[256];
+uint8_t internalBuffer[32];
 /*---------------------------------------------------------------------------*/
 t100::t100()
-{	
-	this->t100_handle = NULL;
+{ 
+  this->t100_handle = NULL;
 
-	this->t100_totalDevices = 0;
+  this->t100_totalDevices = 0;
 }
 /*---------------------------------------------------------------------------*/
 int t100::connectBasic()
 {
-	this->t100_handle = hid_open(VID, PID, NULL);
-	
-	if(!(this->t100_handle))
-	{		
- 		return -1;
-	}	
-	else
-	{
-		return 0;
-	}
+  this->t100_handle = hid_open(VID, PID, NULL);
+  
+  if(!(this->t100_handle))
+  {   
+    return -1;
+  } 
+  else
+  {
+    return 0;
+  }
 }
 /*---------------------------------------------------------------------------*/
 int t100::connectBySerial(int serial)
 {
-	wchar_t buf[16];
+  wchar_t buf[16];
     swprintf(buf, sizeof(buf) / sizeof(*buf), L"%d", serial);
 
     this->t100_handle = hid_open(VID, PID, buf);
-	
-	if(!(this->t100_handle))
-	{		
- 		return -1;
-	}	
-	else
-	{
-		return 0;
-	}
+  
+  if(!(this->t100_handle))
+  {   
+    return -1;
+  } 
+  else
+  {
+    return 0;
+  }
 }
 /*---------------------------------------------------------------------------*/
 int t100::searchDevices()
-{	
-	// Enumerate and print the HID devices on the system
-	struct hid_device_info *devs, *cur_dev;
+{ 
+  // Enumerate and print the HID devices on the system
+  struct hid_device_info *devs, *cur_dev;
 
-	t100_totalDevices = 0;
-	
-	devs = hid_enumerate(0x0, 0x0);
-	cur_dev = devs;	
-	while (cur_dev) {
-		
-		if((cur_dev->vendor_id == VID) && (cur_dev->product_id == PID))
-		{			
-			t100_deviceSerials[t100_totalDevices] = wcstoul(cur_dev->serial_number,NULL,0);
-			t100_totalDevices++;
-		}		
-		cur_dev = cur_dev->next;
-	}
-	hid_free_enumeration(devs);
+  t100_totalDevices = 0;
+  
+  devs = hid_enumerate(0x0, 0x0);
+  cur_dev = devs; 
+  while (cur_dev) 
+  { 
+    if((cur_dev->vendor_id == VID) && (cur_dev->product_id == PID))
+    {     
+      t100_deviceSerials[t100_totalDevices] = wcstoul(cur_dev->serial_number,NULL,0);
+      t100_totalDevices++;
+    }   
+    cur_dev = cur_dev->next;
+  }
+  hid_free_enumeration(devs);
 
   return t100_totalDevices;
 }
 /*---------------------------------------------------------------------------*/
 int t100::getSerialNumber(uint8_t arrayIndex)
 {
-	if(arrayIndex >= t100_totalDevices)
-	{
-		return -1;
-	}
-	else
-	{
-		return t100_deviceSerials[arrayIndex];
-	}	
+  if(arrayIndex >= t100_totalDevices)
+  {
+    return -1;
+  }
+  else
+  {
+    return t100_deviceSerials[arrayIndex];
+  } 
 }
 /*---------------------------------------------------------------------------*/
 int t100::sendData(uint8_t* buf, uint8_t len)
 {
-	int rval;
+  int rval;
 
-	/* Reset the buffer */
-	memset(tempBuffer,0x00,256);
+  /* Reset the buffer */
+  memset(tempBuffer,0x00,256);
 
-	/* Dummy report ID */
-	tempBuffer[0] = 1;
+  /* Dummy report ID */
+  tempBuffer[0] = 1;
 
-	/* Store the data */
-	for(int i = 0; i < len; ++i)
-	{
-		tempBuffer[i+1] = buf[i];
-	}
+  /* Store the data */
+  for(int i = 0; i < len; ++i)
+  {
+    tempBuffer[i+1] = buf[i];
+  }
 
-	/* Send the data */
-	rval = hid_send_feature_report(this->t100_handle, tempBuffer, len+1);
+  /* Send the data */
+  rval = hid_send_feature_report(this->t100_handle, tempBuffer, len+1);
 
-	if(rval < 0)
-	{
-		return -1;
-	}
+  if(rval < 0)
+  {
+    return -1;
+  }
 
-	return (rval-1);
+  return (rval-1);
 }
 /*---------------------------------------------------------------------------*/
 int t100::readData(uint8_t* buf,uint8_t len)
 {
-	int rval;
+  int rval;
 
-	/* Reset the buffer */
-	memset(tempBuffer,0x00,256);
+  /* Reset the buffer */
+  memset(tempBuffer,0x00,256);
 
-	/* Read the data */
-	rval = hid_get_feature_report(this->t100_handle, tempBuffer, len+1);
+  /* Read the data */
+  rval = hid_get_feature_report(this->t100_handle, tempBuffer, len+1);
 
-	if(rval < 0)
-	{
-		return -1;
-	}
-	
-	/* Store the data */
-	for(int i = 1; i < rval; ++i)
-	{
-		buf[i-1] = tempBuffer[i];
-	}
+  if(rval < 0)
+  {
+    return -1;
+  }
+  
+  /* Store the data */
+  for(int i = 1; i < rval; ++i)
+  {
+    buf[i-1] = tempBuffer[i];
+  }
 
-	return (rval-1);
+  return (rval-1);
+}
+/*---------------------------------------------------------------------------*/
+int t100::periodicUpdate()
+{
+  int rval;
+
+  /* Get update command */
+  internalBuffer[0] = 1;     
+  rval = sendData(internalBuffer,32);
+
+  if(rval < 0)
+  {  
+    return -1;
+  }   
+
+  /* Get actual data */
+  rval = readData(internalBuffer,32);
+
+  if(rval < 0)
+  {
+    return -1;
+  }
+
+}
+/*---------------------------------------------------------------------------*/
+float t100::getColdJunctionTemperature()
+{
+  int16_t tmp16;
+  float temperature;
+
+  /* Convert raw data to celsius */
+  tmp16 = (internalBuffer[4] << 8) | internalBuffer[5];
+  tmp16 = tmp16 >> 4;
+  temperature = tmp16 * 0.0625;
+
+  return temperature;
+}
+/*---------------------------------------------------------------------------*/
+float t100::getAdcVoltage()
+{
+  int32_t tmp32;
+  float microvolts;
+  float milivolts;
+
+  /* Make 24bit raw data from 8bit packets */
+  tmp32 = internalBuffer[2] + (internalBuffer[1] << 8) + (internalBuffer[0] << 16);
+
+  /* Mask only relevant 18 bits */  
+  tmp32 = tmp32 & 0x3FFFF;
+
+  /* 2s compliment signed conversion */
+  if (tmp32 & (1<<17))
+  {
+    tmp32 -= 0x3FFFF;
+  }
+
+  /* TODO: ADC step size _will_ change based on the PGA setting! */
+  microvolts = tmp32 * 15.625;
+  milivolts = microvolts / 1000.0;
+
+  return milivolts;
 }
 /*---------------------------------------------------------------------------*/
--- commandline/t100_lib/t100_lib.h
+++ commandline/t100_lib/t100_lib.h
@@ -17,74 +17,101 @@
 
 class t100
 {
-	private:	
-	
-	int t100_totalDevices;
+  private:  
+  
+  int t100_totalDevices;
 
-	hid_device* t100_handle;	
+  hid_device* t100_handle;  
 
-	int t100_deviceSerials[MAXIMUM_T100_DEVICES];	
+  int t100_deviceSerials[MAXIMUM_T100_DEVICES];   
 
-	public:
-	
-	/**
-	 * Simple Constructor
-	 *
-	 * @param (none)
-	 * @return (none)
-	 */
-	t100();
+  public:
+  
+  /**
+   * Simple Constructor
+   *
+   * @param (none)
+   * @return (none)
+   */
+  t100();
 
-	/**
-	  * Try to connect to first t100 device it could find, without any serial 
-	  * number matching.
-	  * 
-	  * @param (none)
-	  * @return 0 for success and -1 for error
-	  */
-	int connectBasic();
+  /**
+    * Try to connect to first t100 device it could find, without any serial 
+    * number matching.
+    * 
+    * @param (none)
+    * @return 0 for success and -1 for error
+    */
+  int connectBasic();
 
-	/**
-	  * Try to connect to specific t100 device with special serial number.	  
-	  * 
-	  * @param serial Specific serial number you want 
-	  * @return 0 for success and -1 for error
-	  */
-	int connectBySerial(int serial);
+  /**
+    * Try to connect to specific t100 device with special serial number.    
+    * 
+    * @param serial Specific serial number you want 
+    * @return 0 for success and -1 for error
+    */
+  int connectBySerial(int serial);
 
-	/**
-	  * Search t100 devices around USB bus and fill t100_deviceSerials array.
-	  * 
-	  * @param (none)
-	  * @return Number of devices found for success and -1 for error
-	  */
-	int searchDevices();
+  /**
+    * Search t100 devices around USB bus and fill t100_deviceSerials array.
+    * 
+    * @param (none)
+    * @return Number of devices found for success and -1 for error
+    */
+  int searchDevices();
 
-	/**
-	  * Access t100_deviceSerials array from your application. 
-	  * 
-	  * @param t100_deviceSerials array index
-	  * @return Serial number of the device. If there isn't any device serial 
-	  * at that location, function returns -1
-	  */
-	int getSerialNumber(uint8_t arrayIndex);
+  /**
+    * Access t100_deviceSerials array from your application. 
+    * 
+    * @param t100_deviceSerials array index
+    * @return Serial number of the device. If there isn't any device serial 
+    * at that location, function returns -1
+    */
+  int getSerialNumber(uint8_t arrayIndex);
 
-	/**
-	  * Low level data sending function
-	  * 
-	  * @param buf Data buffer to be sent
-	  * @param len Amount of bytes to be sent
-	  * @return This function returns the actual number of bytes written and -1 on error
-	  */
-	int sendData(uint8_t* buf, uint8_t len);
+  /**
+    * Low level data sending function
+    * 
+    * @param buf Data buffer to be sent
+    * @param len Amount of bytes to be sent
+    * @return This function returns the actual number of bytes written and -1 on error
+    */
+  int sendData(uint8_t* buf, uint8_t len);
 
-	/**
-	  * Low level data receiving function
-	  * 
-	  * @param buf Data buffer to be filled
-	  * @param len Amount of bytes to be read
-	  * @return This function returns the actual number of bytes read or -1 on error
-	  */
-	int readData(uint8_t* buf, uint8_t len);
+  /**
+    * Low level data receiving function
+    * 
+    * @param buf Data buffer to be filled
+    * @param len Amount of bytes to be read
+    * @return This function returns the actual number of bytes read or -1 on error
+    */
+  int readData(uint8_t* buf, uint8_t len);
+
+  /**
+    * Periodic update function. Call this method to fetch the current temperature 
+    * values to internal buffer. Use other methods to create 'human readable' data 
+    * from that buffer.
+    *
+    * @param (none)
+    * @return 0 for success and -1 for error
+    */
+  int periodicUpdate();
+
+  /**
+    * Use this to check the MCP9800 temperature data
+    *
+    * @param (none)
+    * @return Cold junction temperature value in celsius
+    */
+  float getColdJunctionTemperature();
+
+  /**
+    * Use this to check the MCP3421 voltage reading. This methods converts the raw
+    * data to voltage based on the current PGA setting.
+    *
+    * @param (none)
+    * @return ADC value in milivolts
+    */
+  float getAdcVoltage();
 
 };
